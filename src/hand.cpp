7209e863046cd8d2d785c06beca7f29fb429d274
ZihanQin <zihanqin2048@gmail.com>
2025-08-26 11:25:32 +0000

    feat(sdk): Implement write

--- src/hand.cpp
+++ src/hand.cpp
@@ -38,16 +38,7 @@ public:
 
     template <typename Data>
     py::object read_async() {
-        constexpr auto data_count = []() constexpr {
-            if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Hand>)
-                return 1;
-            else if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Finger>)
-                return 4;
-            else if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Joint>)
-                return 5 * 4;
-        };
-
-        auto context = new FutureContext{*this, data_count()};
+        auto context = new FutureContext{*this, data_count<Data>()};
         hand_.read_async<Data>([context](Data::ValueType) {
             if (context->count_down()) {
                 py::gil_scoped_acquire acquire;
@@ -64,6 +55,34 @@ public:
     template <typename Data>
     void read_async_unchecked() {
         hand_.read_async_unchecked<Data>();
+    }
+
+    template <typename Data>
+    auto write(py::numpy_scalar<typename Data::ValueType> value) {
+        py::gil_scoped_release release;
+        hand_.write<Data>(value.value);
+    }
+
+    template <typename Data>
+    py::object write_async(py::numpy_scalar<typename Data::ValueType> value) {
+        auto context = new FutureContext{*this, data_count<Data>()};
+        hand_.write_async<Data>(
+            [context]() {
+                if (context->count_down()) {
+                    py::gil_scoped_acquire acquire;
+                    context->call_threadsafe(context->future.attr("set_result"), py::none());
+                    delete context;
+                }
+            },
+            value.value);
+        hand_.trigger_transmission();
+
+        return context->future;
+    }
+
+    template <typename Data>
+    void write_async_unchecked(py::numpy_scalar<typename Data::ValueType> value) {
+        hand_.write_async_unchecked<Data>(value.value);
     }
 
     void trigger_transmission() { hand_.trigger_transmission(); }
@@ -88,11 +107,19 @@ public:
 
     template <typename Data>
     static void register_py_interface(py::class_<Hand>& py_class, const std::string& name) {
-        py_class.def(("read_" + name).c_str(), &Hand::read<Data>);
-        py_class.def(("read_" + name + "_async").c_str(), &Hand::read_async<Data>);
-        py_class.def(
-            ("read_" + name + "_async_unchecked").c_str(), &Hand::read_async_unchecked<Data>);
-        py_class.def(("get_" + name).c_str(), &Hand::get<Data>);
+        if constexpr (Data::readable) {
+            py_class.def(("read_" + name).c_str(), &Hand::read<Data>);
+            py_class.def(("read_" + name + "_async").c_str(), &Hand::read_async<Data>);
+            py_class.def(
+                ("read_" + name + "_async_unchecked").c_str(), &Hand::read_async_unchecked<Data>);
+            py_class.def(("get_" + name).c_str(), &Hand::get<Data>);
+        }
+        if constexpr (Data::writable) {
+            py_class.def(("write_" + name).c_str(), &Hand::write<Data>);
+            py_class.def(("write_" + name + "_async").c_str(), &Hand::write_async<Data>);
+            py_class.def(
+                ("write_" + name + "_async_unchecked").c_str(), &Hand::write_async_unchecked<Data>);
+        }
     }
 
 private:
@@ -118,6 +145,16 @@ private:
         py::object future;
     };
 
+    template <typename Data>
+    static constexpr int data_count() {
+        if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Hand>)
+            return 1;
+        else if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Finger>)
+            return 4;
+        else if constexpr (std::is_same_v<typename Data::Base, wujihandcpp::device::Joint>)
+            return 5 * 4;
+    };
+
     wujihandcpp::device::Hand hand_;
 };
 
@@ -128,13 +165,22 @@ PYBIND11_MODULE(wujihandpy, m) {
     hand.def(
         py::init<uint16_t, int32_t>(), py::arg("usb_vid") = 0x0483, py::arg("usb_pid") = 0x5740);
 
-    Hand::register_py_interface<wujihandcpp::data::hand::FirmwareVersion>(hand, "firmware_version");
-    Hand::register_py_interface<wujihandcpp::data::hand::FirmwareDate>(hand, "firmware_date");
-    Hand::register_py_interface<wujihandcpp::data::hand::SystemTime>(hand, "system_time");
-    Hand::register_py_interface<wujihandcpp::data::hand::McuTemperature>(hand, "mcu_temperature");
-    Hand::register_py_interface<wujihandcpp::data::hand::InputVoltage>(hand, "input_voltage");
+    using namespace wujihandcpp;
+    Hand::register_py_interface<data::hand::FirmwareVersion>(hand, "firmware_version");
+    Hand::register_py_interface<data::hand::FirmwareDate>(hand, "firmware_date");
+    Hand::register_py_interface<data::hand::SystemTime>(hand, "system_time");
+    Hand::register_py_interface<data::hand::McuTemperature>(hand, "mcu_temperature");
+    Hand::register_py_interface<data::hand::InputVoltage>(hand, "input_voltage");
 
-    Hand::register_py_interface<wujihandcpp::data::joint::Position>(hand, "joint_position");
+    Hand::register_py_interface<data::hand::PdoEnabled>(hand, "pdo_enabled");
+    Hand::register_py_interface<data::hand::GlobalTpdoId>(hand, "global_tpdo_id");
+    Hand::register_py_interface<data::hand::JointPdoInterval>(hand, "joint_pdo_interval");
+
+    Hand::register_py_interface<data::joint::ControlMode>(hand, "joint_control_mode");
+    Hand::register_py_interface<data::joint::SinLevel>(hand, "joint_sin_level");
+    Hand::register_py_interface<data::joint::ControlWord>(hand, "joint_control_word");
+    Hand::register_py_interface<data::joint::Position>(hand, "joint_position");
+    Hand::register_py_interface<data::joint::ControlPosition>(hand, "joint_control_position");
 
     hand.def("trigger_transmission", &Hand::trigger_transmission);
 
